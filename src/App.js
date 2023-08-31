ecdf1c7f073bc251c48a446635dd2ffc1ba619be
amirrajabzadeh <developer908908@gmail.com>
2023-08-31 22:01:26 +0330

    Create Project And add Tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <div className="basis-[12%] border">1</div>
+      <div className="basis-[88%] border">2</div>
     </div>
   );
 }
---
+++ src/components/Slidebar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Slidebar = () => {
+  return (
+    <div>Slidebar</div>
+  )
+}
