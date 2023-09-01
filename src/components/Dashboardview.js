34f06b77544ded293a8c987d0ab320435d834cc9
amirrajabzadeh <developer908908@gmail.com>
2023-09-01 10:30:08 +0330

    add Dashboardview and Search Input

--- src/components/Dashboardview.js
+++ src/components/Dashboardview.js
@@ -4,7 +4,16 @@ import { FaSearch } from "react-icons/fa";
 const Dashboardview = () => {
   return (
     <div>
-      
+      <div className="flex items-center rounded-[5px] ">
+        <input
+          type="text"
+          className="bg-[#F8F9FC] h-[40px] outline-none pl-[13px] w-[350px] rounded-[5px] placeholder:text-[14px] leading-[20px] font-normal "
+          placeholder="Search for ..."
+        />
+        <div className="bg-[#4E73DF] h-[40px] px-[14px] flex items-center justify-center rounded-tr-[5px] rounded-br-[5px] ">
+          <FaSearch color="white" />
+        </div>
+      </div>
     </div>
   );
 };
