8fc8f2ec1bccc31841b73656bb930885973d13d9
amirrajabzadeh <developer908908@gmail.com>
2023-09-01 15:54:56 +0330

    Add Router and use

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "../App";
+import Main from "../components/Main";
 
 const Router = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<App />}></Route>
+          <Route path="/" element={<App />}>
+            <Route index element={<Main />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
